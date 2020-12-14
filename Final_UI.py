9243107851e80512bd99e10a9f7baba4548a22ba
Josungjun <66383669+Josungjun@users.noreply.github.com>
2020-12-14 23:08:36 +0900

    Update Final_UI.py

--- Final_UI.py
+++ Final_UI.py
@@ -1,184 +1,233 @@
+import pygame
 import sys
-from PyQt5.QtWidgets import *
-from PyQt5.QtCore import Qt
+from time import sleep
+from pygame.locals import *
+import random
 
-import PyshootingR
-class Shoot(QWidget):
+Black = (0, 0, 0)
+WHITE = (255, 255, 255)
+YELLOW = (250, 250, 50)
+RED = (250, 50, 50)
 
-    def __init__(self):
-        super().__init__()
-        self.initUI()
+WINDOW_WIDTH = 480
+WINDOW_HEIGHT = 600
 
+FPS = 60
 
-    def initUI(self):
-        self.ReadyWindow = QTextEdit()
-        self.ReadyWindow.setReadOnly(True)
-        self.ReadyWindow.setAlignment(Qt.AlignLeft)
-        self.ReadyWindow.setText("이동 : 좌우 방향키 \n"
-                                 "미사일 발사 : 스패이스바 \n"
-                                 "easy 모드 : \n"
-                                 "Normal 모드 : \n"
-                                 "Hard 모드 : \n"
-                                 "1번 비행기 : \n"
-                                 "2번 비행기 : \n")
-        self.ReadyWindowT = QLabel()
-        self.ReadyWindowT.setText("기본 환경 설정 설명")
-        GameReadyLayout = QGridLayout()
-        GameReadyLayout.addWidget(self.ReadyWindowT, 0, 0)
-        GameReadyLayout.addWidget(self.ReadyWindow, 1, 0)
+class Fighter(pygame.sprite.Sprite):
+    def __init__(self, Fly):
+        super(Fighter, self).__init__()
+        self.image = pygame.image.load(Fly)
+        self.rect = self.image.get_rect()
+        self.rect.x = int(WINDOW_WIDTH / 2)
+        self.rect.y = WINDOW_HEIGHT - self.rect.height
+        self.dx = 0
+        self.dy = 0
 
-        self.startbutton = QPushButton('Start')
-        self.quitbutton = QPushButton('Quit')
+    def update(self):
+        self.rect.x += self.dx
+        if self.rect.x < 0 or self.rect.x + self.rect.width > WINDOW_WIDTH:
+            self.rect.x -= self.dx
 
-        settingGrid = QGridLayout()
-        settingGrid.addWidget(self.difficulty(), 0, 0)
-        settingGrid.addWidget(self.ship(), 1, 0)
-        settingGrid.addWidget(self.background(), 0, 1)
-        settingGrid.addWidget(self.music(), 1, 1)
-        settingGrid.addWidget(self.button(), 0, 2)
+        if self.rect.y < 0 or self.rect.y + self.rect.height > WINDOW_HEIGHT:
+            self.rect.y -= self.dy
 
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
 
-        mainLayout = QGridLayout()
-        mainLayout.addLayout(GameReadyLayout, 0, 0)
-        mainLayout.addLayout(settingGrid, 1, 0)
+    def collide(self, sprites):
+        for sprite in sprites:
+            if pygame.sprite.collide_rect(self, sprite):
+                return sprite
 
 
-        self.setLayout(mainLayout)
-        self.setWindowTitle("Shooting")
+class Missile(pygame.sprite.Sprite):
+    def __init__(self, xpos, ypos, speed):
+        super(Missile, self).__init__()
+        self.image = pygame.image.load('missile.png')
+        self.rect = self.image.get_rect()
+        self.rect.x = xpos
+        self.rect.y = ypos
+        self.speed = speed
+        self.sound = pygame.mixer.Sound('missile.wav')
+
+    def launch(self):
+        self.sound.play()
+
+    def update(self):
+        self.rect.y -= self.speed
+        if self.rect.y + self.rect.height < 0:
+            self.kill()
+
+    def collide(self, sprites):
+        for sprite in sprites:
+            if pygame.sprite.collide_rect(self, sprite):
+                return sprite
 
 
-    def difficulty(self):
-        groupbox = QGroupBox('난이도')
+class Rock(pygame.sprite.Sprite):
+    def __init__(self, xpos, ypos, speed):
+        super(Rock, self).__init__()
+        rock_image = ('rock.png')
+        self.image = pygame.image.load(rock_image)
+        self.rect = self.image.get_rect()
+        self.rect.x = xpos
+        self.rect.y = ypos
+        self.speed = speed
 
-        radio1 = QRadioButton('easy')
-        radio2 = QRadioButton('normal')
-        radio3 = QRadioButton('hard')
-        radio1.clicked.connect(self.buttonClicked)
-        radio2.clicked.connect(self.buttonClicked)
-        radio3.clicked.connect(self.buttonClicked)
-        radio1.setChecked(True)
+    def update(self):
+        self.rect.y += self.speed
 
-        vbox = QVBoxLayout()
-        vbox.addWidget(radio1)
-        vbox.addWidget(radio2)
-        vbox.addWidget(radio3)
-        groupbox.setLayout(vbox)
+    def out_of_screen(self):
+        if self.rect.y > WINDOW_HEIGHT:
+            return True
 
-        return groupbox
+def draw_text(text, font, surface, x, y, main_color):
+    text_obj = font.render(text, True, main_color)
+    text_rect = text_obj.get_rect()
+    text_rect.centerx = x
+    text_rect.centery = y
+    surface.blit(text_obj, text_rect)
 
-    def ship(self):
-        groupbox = QGroupBox('비행기')
+def occur_explosion(surface, x, y):
+    explosion_image = pygame.image.load('explosion.png')
+    explosion_rect = explosion_image.get_rect()
+    explosion_rect.x = x
+    explosion_rect.y = y
+    surface.blit(explosion_image, explosion_rect)
 
-        radio1 = QRadioButton('starwars')
-        radio2 = QRadioButton('likeBug')
-        radio1.clicked.connect(self.buttonClicked)
-        radio2.clicked.connect(self.buttonClicked)
+    explosion_sounds = ('explosion.wav')
+    explosion_sound = pygame.mixer.Sound(explosion_sounds)
+    explosion_sound.play()
 
-        radio1.setChecked(True)
+def game(background, max_speed, min_speed, occurrock, occurock2, Fly, music):
+    default_font = pygame.font.SysFont('suruma', 25)
+    background_image = pygame.image.load(background)
+    gameover_sound = pygame.mixer.Sound('gameover.wav')
+    pygame.mixer.music.load(music)
+    pygame.mixer.music.play(-1)
+    fps_clock = pygame.time.Clock()
 
-        vbox = QVBoxLayout()
-        vbox.addWidget(radio1)
-        vbox.addWidget(radio2)
+    fighter = Fighter(Fly)
+    missiles = pygame.sprite.Group()
+    rocks = pygame.sprite.Group() #많은 객체들을 효과적으로 다뤄주는 pygame 함수
 
-        groupbox.setLayout(vbox)
+    occur_prob = 40
+    shot_count = 0
+    count_missed = 0
 
-        return groupbox
+    done = False
+    while not done:
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    fighter.dx -= 5
+                elif event.key == pygame.K_RIGHT:
+                    fighter.dx += 5
+                elif event.key == pygame.K_SPACE:
+                    missile = Missile(fighter.rect.centerx, fighter.rect.y, 10)
+                    missile.launch()
+                    missiles.add(missile)
+                elif event.key == pygame.K_ESCAPE:
+                    pygame.mixer.music.stop()
+                    return game_menu()
+            if event.type == pygame.QUIT:
+                return game_menu()
 
-    def background(self):
-        groupbox = QGroupBox('배경')
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                    fighter.dx = 0
 
-        radio1 = QRadioButton('성운')
-        radio2 = QRadioButton('우주인')
-        radio3 = QRadioButton('별들')
-        radio1.clicked.connect(self.buttonClicked)
-        radio2.clicked.connect(self.buttonClicked)
-        radio3.clicked.connect(self.buttonClicked)
-        radio1.setChecked(True)
+        screen.blit(background_image, background_image.get_rect())
 
-        vbox = QVBoxLayout()
-        vbox.addWidget(radio1)
-        vbox.addWidget(radio2)
-        vbox.addWidget(radio3)
-        groupbox.setLayout(vbox)
+        occur_of_rocks = occurock2 + int(shot_count / occurrock)
+        min_rock_speed = 1 + int(shot_count / min_speed)
+        max_rock_speed = 1 + int(shot_count / max_speed)
 
-        return groupbox
+        if random.randint(1, occur_prob) == 1:
+            for i in range(occur_of_rocks):
+                speed = random.randint(min_rock_speed, max_rock_speed)
+                rock = Rock(random.randint(0, WINDOW_WIDTH - 30), 0 , speed)
+                rocks.add(rock)
 
-    def music(self):
-        groupbox = QGroupBox('음악')
 
-        radio1 = QRadioButton('잔잔')
-        radio2 = QRadioButton('funny')
-        radio3 = QRadioButton('신남')
-        radio1.clicked.connect(self.buttonClicked)
-        radio2.clicked.connect(self.buttonClicked)
-        radio3.clicked.connect(self.buttonClicked)
-        radio1.setChecked(True)
+        draw_text('detroyed rocks : {}'.format(shot_count), default_font, screen, 100, 20, YELLOW)
+        draw_text('lose rocks : {}'.format(count_missed), default_font, screen, 400, 20, RED)
 
-        vbox = QVBoxLayout()
-        vbox.addWidget(radio1)
-        vbox.addWidget(radio2)
-        vbox.addWidget(radio3)
-        groupbox.setLayout(vbox)
+        for missile in missiles:
+            rock = missile.collide(rocks)
+            if rock:
+                missile.kill()
+                rock.kill()
+                occur_explosion(screen, rock.rect.x, rock.rect.y)
+                shot_count += 1
 
-        return groupbox
+        for rock in rocks:
+            if rock.out_of_screen():
+                rock.kill()
+                count_missed += 1
 
-    def button(self):
-        groupbox = QGroupBox()
+        rocks.update()
+        rocks.draw(screen)
+        missiles.update()
+        missiles.draw(screen)
+        fighter.update()
+        fighter.draw(screen)
+        pygame.display.flip()
 
-        startbutton = QPushButton('Start')
-        quitbutton = QPushButton('Quit')
-        startbutton.clicked.connect(self.buttonClicked)
-        quitbutton.clicked.connect(self.buttonClicked)
-        vbox = QVBoxLayout()
-        vbox.addWidget(startbutton)
-        vbox.addWidget(quitbutton)
-        groupbox.setLayout(vbox)
+        if fighter.collide(rocks) or count_missed >= 3:
+            pygame.mixer_music.stop()
+            occur_explosion(screen, fighter.rect.x, fighter.rect.y)
+            pygame.display.update()
+            gameover_sound.play()
+            sleep(1)
+            done = True
 
-        return groupbox
+        fps_clock.tick(FPS)
 
-    def buttonClicked(self):
-        global max_speed, min_speed, background, occurrock, occurrock2, Fly, music
+    return 'game_menu'
 
-        button = self.sender()
-        key = button.text()
-        if key == '성운':
-            background = "Nebula.jpg"
-        elif key == '우주인':
-            background = "Astronaut.jpg"
-        elif key == '별들':
-            background = "Star.jpg"
-        elif key == 'starwars':
-            Fly = 'fighterStar.png'
-        elif key == 'likeBug':
-            Fly = 'fighterBug.png'
-        elif key == 'easy':
-            max_speed = 200
-            min_speed = 300
-            occurrock = 200
-            occurrock2 = 1
-        elif key == 'normal':
-            max_speed = 100
-            min_speed = 200
-            occurrock = 150
-            occurrock2 = 2
-        elif key == 'hard':
-            max_speed = 50
-            min_speed = 100
-            occurrock = 100
-            occurrock2 = 3
-        elif key == '잔잔':
-            music = "inspire.wav"
-        elif key == 'funny':
-            music = "funny.wav"
-        elif key == '신남':
-            music = "extreme.wav"
-        elif key == 'Start':
-            PyshootingR.main(background, max_speed, min_speed, occurrock, occurrock2, Fly, music)
-        elif key == 'Quit':
-            sys.exit()
+def game_menu():
+    start_image = pygame.image.load('fantasy.jpg')
+    screen.blit(start_image, [0,0])
+    draw_x = int(WINDOW_WIDTH / 2)
+    draw_y = int(WINDOW_HEIGHT / 4)
+    font_70 = pygame.font.SysFont('suruma', 70)
+    font_40 = pygame.font.SysFont('suruma', 40)
+
+    draw_text('ShotingGame', font_70, screen, draw_x, draw_y, Black)
+    draw_text('press enter', font_40, screen, draw_x, draw_y + 200, WHITE)
+    draw_text('Start game', font_40, screen, draw_x, draw_y + 250, WHITE)
+
+    pygame.display.update()
+
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RETURN:
+                return 'play'
+            elif event.key == pygame.K_ESCAPE:
+                return 'quit'
+        if event.type == QUIT:
+            return 'quit'
+
+    return 'game_menu'
+
+def main(background, max_speed, min_speed, occurrock, occurrock2, Fly, music):
+    global screen
+
+    pygame.init()
+    screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+    pygame.display.set_caption('PyShooting')
+
+    action = 'game_menu'
+    while action != 'quit':
+        if action == 'game_menu':
+            action = game_menu()
+        elif action == 'play':
+            action = game(background, max_speed, min_speed, occurrock, occurrock2, Fly, music)
+
+    pygame.quit()
 
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    shoot = Shoot()
-    shoot.show()
-    app.exec_()
+    main()
+
